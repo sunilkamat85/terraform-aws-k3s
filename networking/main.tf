2546b2c53e0f7c587214027716b48e7b1094f00b
sunilkamatec@gmail.com <ubuntu@ip-172-31-25-132.us-east-2.compute.internal>
2021-11-03 09:47:31 +0000

    lifecycle  create_before_destroy for vpc

--- networking/main.tf
+++ networking/main.tf
@@ -21,6 +21,10 @@ resource "aws_vpc" "mtc_vpc" {
     tags = {
         Name = "mtc_vpc-${random_integer.random.id}"
     }
+    
+    lifecycle {
+        create_before_destroy = true
+    }
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
